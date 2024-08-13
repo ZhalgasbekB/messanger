42eeb5e3818ecfefd390491f33348726ce420e21
zhbolatov <gfk575@gmail.com>
2024-08-13 15:06:53 +0500

    3 ???

--- internal/models/messager.go
+++ internal/models/messager.go
@@ -1,7 +1,17 @@
 package models
 
-type (
-	Messanger     struct{}
-	MessangerDTO  struct{}
-	MessangerDTO1 struct{}
-)
+import "time"
+
+type Messanger struct {
+	ID             int       `json:"id"`
+	ConversationID int       `json:"room_id"`
+	UserID         int       `json:"user_id"`
+	Message        string    `json:"message"`
+	CreatedAt      time.Time `json:created_at"`
+}
+
+type Conversations struct {
+	ID int
+}
+
+type Members struct{}
