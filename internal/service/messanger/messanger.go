32f5b7b4486a0eb0435cca4b66ec555bba1cd004
zhbolatov <gfk575@gmail.com>
2024-08-14 10:58:00 +0500

    FDFD

--- internal/service/messanger/messanger.go
+++ internal/service/messanger/messanger.go
@@ -1,1 +1,32 @@
 package messanger
+
+import (
+	"forum/internal/models"
+	repo "forum/internal/repository"
+)
+
+type ConversationService struct {
+	Conversation repo.Conversation
+}
+
+func NewMessangerService(repo repo.Repository) *ConversationService {
+	return &ConversationService{
+		Conversation: repo,
+	}
+}
+
+func (con *ConversationService) ConversationCreateService(conversation *models.Conversations) error {
+	return con.Conversation.ConversationCreate(conversation)
+}
+
+func (con *ConversationService) ConversationsService() ([]*models.Conversations, error) {
+	return con.Conversation.Conversations()
+}
+
+func (con *ConversationService) ConversationHistoryService(conversation_id int) ([]*models.Messanger, error) {
+	return con.Conversation.ConversationHistory(conversation_id)
+}
+
+func (con *ConversationService) SendMessageService(message models.Messanger) error {
+	return con.Conversation.SendMessage(message)
+}
